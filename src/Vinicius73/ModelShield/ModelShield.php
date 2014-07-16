47102e987c543402c867d23f756fa18877ebdce5
Vinicius <luiz.vinicius73@gmail.com>
2014-07-16 12:40:43 -0300

    reindent

--- src/Vinicius73/ModelShield/ModelShield.php
+++ src/Vinicius73/ModelShield/ModelShield.php
@@ -6,103 +6,107 @@ use Validator;
 
 class ModelShield extends Model
 {
-	/**
-	 * Rules for validation
-	 *
-	 * @var array
-	 */
-	public static $rules = array();
-	/**
-	 * Rules for validation on update
-	 *
-	 * @var array
-	 */
-	public static $rulesUpdate = array();
-	/**
-	 * @var \Illuminate\Support\MessageBag
-	 */
-	protected $validationErrors;
+    /**
+     * Rules for validation
+     *
+     * @var array
+     */
+    public static $rules = array();
+    /**
+     * Rules for validation on update
+     *
+     * @var array
+     */
+    public static $rulesUpdate = array();
+    /**
+     * @var \Illuminate\Support\MessageBag
+     */
+    protected $validationErrors;
 
-	public function __construct(array $attributes = array())
-	{
-		parent::__construct($attributes);
-		$this->validationErrors = new MessageBag();
-	}
-
-
-	/**
-	 * @return bool
-	 */
-	public function isValid()
-	{
-		return (boolean)$this->validate();
-	}
-
-	/**
-	 * Provides validation rules
-	 *
-	 * @return array
-	 */
-	public function getRules()
-	{
-		if ($this->exists and !empty(static::$rulesUpdate)):
-			return static::$rulesUpdate;
-		endif;
-
-		return static::$rules;
-	}
+    public function __construct(array $attributes = array())
+    {
+        parent::__construct($attributes);
+        $this->validationErrors = new MessageBag();
+    }
 
 
-	public function save(array $options = array())
-	{
-		$isValid = $this->isValid();
+    /**
+     * @return bool
+     */
+    public function isValid()
+    {
+        return (boolean)$this->validate();
+    }
 
-		if ($isValid):
-			return parent::save($options);
-		endif;
+    /**
+     * Provides validation rules
+     *
+     * @return array
+     */
+    public function getRules()
+    {
+        if ($this->exists and !empty(static::$rulesUpdate)):
+            return static::$rulesUpdate;
+        endif;
 
-		return FALSE;
-	}
+        return static::$rules;
+    }
 
-	/**
-	 * Validates the model
-	 *
-	 * @return bool
-	 */
-	protected function validate()
-	{
-		$rules      = $this->getRules();
-		$attributes = $this->prepareAttributes();
-		$validator  = Validator::make($attributes, $rules);
-		$success    = $validator->passes();
 
-		if (!$success):
-			$this->setErrors($validator->errors());
-		endif;
+    public function save(array $options = array())
+    {
+        $isValid = $this->isValid();
 
-		return $success;
-	}
+        if ($isValid):
+            return parent::save($options);
+        endif;
 
-	/**
-	 * Prepare the attributes of the model before being validated
-	 *
-	 * @return array
-	 */
-	protected function prepareAttributes()
-	{
-		return $this->getAttributes();
-	}
+        return false;
+    }
 
-	/**
-	 * @param MessageBag $errors
-	 */
-	protected function setErrors(MessageBag $errors)
-	{
-		$this->validationErrors = $errors;
-	}
+    /**
+     * Validates the model
+     *
+     * @return bool
+     */
+    protected function validate()
+    {
+        $rules      = $this->getRules();
+        $attributes = $this->prepareAttributes();
+        $validator  = Validator::make($attributes, $rules);
+        $success    = $validator->passes();
 
-	public function getErrors()
-	{
-		return $this->validationErrors;
-	}
+        if (!$success):
+            $this->setErrors($validator->errors());
+        endif;
+
+        return $success;
+    }
+
+    /**
+     * Prepare the attributes of the model before being validated
+     *
+     * @return array
+     */
+    protected function prepareAttributes()
+    {
+        return $this->getAttributes();
+    }
+
+    /**
+     * @param MessageBag $errors
+     */
+    protected function setErrors(MessageBag $errors)
+    {
+        $this->validationErrors = $errors;
+    }
+
+
+    /**
+     * @return MessageBag
+     */
+    public function getErrors()
+    {
+        return $this->validationErrors;
+    }
 }
